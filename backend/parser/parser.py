ea7a0d60d5261288e657d0901c20126b5a692d4f
rougenn <deziq.hi@gmail.com>
2024-11-09 15:18:33 +0300

    last successful

--- backend/parser/parser.py
+++ backend/parser/parser.py
@@ -3,61 +3,81 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import time
 
 # Функция для парсинга блоков с классом "ui grid"
 def parse_ui_grid_blocks(html):
+    print(html)
     # Парсим HTML с помощью BeautifulSoup
     soup = BeautifulSoup(html, "html.parser")
     
     # Ищем все блоки с классом "ui grid"
     grid_blocks = soup.find_all("div", class_="ui grid")
     
-    # Используем defaultdict для хранения ключ-значение пар с несколькими значениями
+    # Используем defaultdict для хранения ключ-значение пар
     data_dict = defaultdict(list)
     
     for block in grid_blocks:
-        # Находим все элементы с текстом внутри блока
         labels = block.find_all("label")
         values = block.find_all("div", class_="LabeledValue-sc-10trpha-0")
         
-        # Для каждого label находим соответствующее значение
         for label, value in zip(labels, values):
             key = label.get_text(strip=True)
             value_text = value.get_text(strip=True)
-            data_dict[key].append(value_text)  # Добавляем значение в список для данного ключа
+            
+            # Убираем дублирование ключа в значении
+            if value_text.startswith(key):
+                value_text = value_text[len(key):].strip()
+            
+            # Удаляем символы \xa0
+            value_text = value_text.replace('\xa0', '')
+            
+            # Добавляем значение в data_dict
+            data_dict[key].append(value_text)
 
-    return data_dict
+    # Создаем новый словарь, содержащий только каждый второй элемент для каждого ключа
+    filtered_data_dict = {key: values[::2] for key, values in data_dict.items()}
+
+    return filtered_data_dict
+
 
 # Функция для загрузки HTML-кода страницы
 def get_html(driver, url):
     # Открываем страницу
     driver.get(url)
     
-    # Ожидание появления элемента с текстом "Количество" внутри нужного блока
+    # Ожидание появления первого элемента с кнопкой "Показать подробную информацию"
     WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.XPATH, "//div[@class='LabeledValue-sc-10trpha-0 eDzBib']/label[text()='Количество']"))
+        EC.presence_of_element_located((By.XPATH, "//span[@id='show-more-button']"))
     )
+
+    # Находим все кнопки "Показать подробную информацию"
+    show_more_buttons = driver.find_elements(By.XPATH, "//span[@id='show-more-button']")
     
-    # Ожидание изменения структуры страницы
+    # Нажимаем на все кнопки "Показать подробную информацию"
+    for button in show_more_buttons:
+        driver.execute_script("arguments[0].scrollIntoView(true);", button)  # Прокручиваем страницу к кнопке
+        button.click()  # Кликаем по кнопке
+        time.sleep(0.4)  # Ждем 2 секунды, чтобы страница успела обновиться после клика
+
+    # Получаем HTML-код страницы после нажатия на все кнопки
     html = driver.page_source
-    while True:
-        time.sleep(1)
-        new_html = driver.page_source
-        if new_html != html:  # Страница изменилась
-            html = new_html
-        else:
-            break  # Страница больше не меняется, можно продолжать
     return html
 
-def main():
-    # Настройка Firefox-драйвера
-    options = Options()
-    options.headless = True  # Запуск в фоновом режиме, если нужно
 
-    driver = webdriver.Firefox(options=options)
+def main():
+    # Настройка Chrome-драйвера
+    options = Options()
+    options.headless = True  # если хотите запускать в фоновом режиме
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    # Путь к драйверу Chrome
+    service = Service("/usr/local/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=options)
 
     try:
         # URL страницы
@@ -67,7 +87,6 @@ def main():
         html_code = get_html(driver, url)
         
         # Печатаем или сохраняем HTML-код
-        # print(html_code)
         parsed_data = parse_ui_grid_blocks(html_code)
         print(parsed_data)
         
